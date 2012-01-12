f846aa225da63b7ecddac54516602996a66f251b
deoxxa <deoxxa@fknsrs.biz>
2012-01-13 04:51:49 +0900

    added README, updated test script

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
 var fs = require("fs"),
-    util = require("util"),
-    uglify = require("uglify-js").uglify,
     Ginger = require("./lib/ginger");
-
-var data = fs.readFileSync("res/test.twig").toString().replace(/\s+$/, "");
 
 var compiler = new Ginger.Compiler();
 
@@ -20,32 +16,10 @@ var ctx = new Ginger.Context({
 ctx.add_function("default", function(input, args) { return input || args[0]; });
 ctx.add_function("ucwords", function(input, args) { return input.replace(/(^|\s)([a-z])/g, function(m, p1, p2) { return p1 + p2.toUpperCase(); }); });
 
-try {
-  var parsed = Ginger.Parser.parse(data);
-} catch (e) {
-  console.log(e);
-  process.exit();
-}
-
-//console.log(JSON.stringify(parsed, null, 2));
-//console.log("");
-
-//var ast = compiler.make_ast(parsed);
-//console.log(JSON.stringify(ast, null, 2));
-//console.log("");
-
-var compiled = compiler.compile(parsed);
-//console.log(compiled);
-//console.log("");
-
-//console.log(data);
-//console.log("");
-
-ctx.add_template("index", new Function("ctx", compiled));
+ctx.add_template("simple", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/simple.twig").toString()))));
+ctx.add_template("index", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/index.twig").toString()))));
 ctx.add_template("layout", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/layout.twig").toString()))));
 ctx.add_template("header", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/included.twig").toString()))));
 ctx.add_template("footer", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/included.twig").toString()))));
-
-console.log(ctx.get_template("index").toString());
 
 console.log(ctx.render("index"));

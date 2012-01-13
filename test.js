4b97aad81be95bae820bc5143d3b2b50442b8457
deoxxa <deoxxa@fknsrs.biz>
2012-01-14 02:15:22 +0900

    changed file extension from .twig to .ginger

--- test.js
+++ test.js
@@ -16,10 +16,10 @@ var ctx = new Ginger.Context({
 ctx.add_function("default", function(input, args) { return input || args[0]; });
 ctx.add_function("ucwords", function(input, args) { return input.replace(/(^|\s)([a-z])/g, function(m, p1, p2) { return p1 + p2.toUpperCase(); }); });
 
-ctx.add_template("simple", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/simple.twig").toString()))));
-ctx.add_template("index", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/index.twig").toString()))));
-ctx.add_template("layout", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/layout.twig").toString()))));
-ctx.add_template("header", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/included.twig").toString()))));
-ctx.add_template("footer", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/included.twig").toString()))));
+ctx.add_template("simple", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/simple.ginger").toString()))));
+ctx.add_template("index", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/index.ginger").toString()))));
+ctx.add_template("layout", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/layout.ginger").toString()))));
+ctx.add_template("header", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/included.ginger").toString()))));
+ctx.add_template("footer", new Function("ctx", compiler.compile(Ginger.Parser.parse(fs.readFileSync("res/included.ginger").toString()))));
 
 console.log(ctx.render("index"));
